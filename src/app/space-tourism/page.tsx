7ab8acb74238ae2efd6277e66f354bcc167f518a
Devlin Morrow <devlin.glasman@gmail.com>
2024-08-22 18:55:50 +0200

    Add Background img home page

--- src/app/space-tourism/page.tsx
+++ src/app/space-tourism/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import styled from 'styled-components'
+import theme from "tailwindcss/defaultTheme";
 
 const Subtitle = styled.h4`
   color: green;
@@ -9,8 +10,27 @@ const Subtitle = styled.h4`
 export default function SpaceTourism() {
   return (
     <>
-      <Subtitle>HELLO WORLD</Subtitle>
-      <p>Testing</p>
+      <Background>
+        <Subtitle>HELLO WORLD</Subtitle>
+      </Background>
     </>
   )
 }
+
+const Background = styled.main`
+  // MOBILE + DEFAULT STYLES
+  height: 100vh;
+  width: 100vw;
+  background-image: url('/assets/home/background-home-mobile.jpg');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+
+  //@include tablet {
+  //  background-image: url('/assets/home/background-home-tablet.jpg');
+  //}
+
+  //@include desktop {
+  //  background-image: url('/assets/home/background-home-desktop.jpg');
+  //}
+`
