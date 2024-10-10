88c5aceee3438295393ac38ddb2de98e2eea8e14
Claire Nguyen <6858159+clairenguyen@users.noreply.github.com>
2024-10-10 10:14:51 -0700

    Add prettier, redirect from index page, other heading styles

--- src/app/globalStyles.ts
+++ src/app/globalStyles.ts
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
+// TODO: Bellefair Regular - Preset 1, 2, 3, 4, 6
+// TODO: Barlow Condensed Regular - Preset 5, 7, 8, 9
 const GlobalStyle = createGlobalStyle`
 	h1 {
 		font-size: 144px;
@@ -15,6 +17,23 @@ const GlobalStyle = createGlobalStyle`
 		font-size: 56px;
 		line-height: normal;
 	}
-`
 
-export default GlobalStyle
+    h4 {
+      font-size: 32px;
+      line-height: normal;
+    }
+
+    
+    h5 {
+      font-size: 28px;
+      line-height: normal;
+      letter-spacing: 4px;
+    }
+
+    h6 {
+      font-size: 28px;
+      line-height: normal;
+    }
+`;
+
+export default GlobalStyle;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/space-tourism");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>CSS showcase</h1>
+      <div> If you don't see anything, go to /space-tourism</div>
     </main>
   );
 }
