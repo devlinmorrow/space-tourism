cefc422aac301f6fd99ee93d3727d405cf66cc45
Claire Nguyen <6858159+clairenguyen@users.noreply.github.com>
2024-10-16 09:30:44 -0700

    add padding

--- src/app/space-tourism/page.tsx
+++ src/app/space-tourism/page.tsx
@@ -1,46 +1,57 @@
-'use client'
+"use client";
 
-import styled from 'styled-components'
-import { QUERIES } from '../constants'
-import GlobalStyle from '../globalStyles'
+import styled from "styled-components";
+import { QUERIES } from "../constants";
+import GlobalStyle from "../globalStyles";
 
 export default function SpaceTourism() {
   return (
     <>
       <GlobalStyle />
       <Background>
-        <Subtitle>So, you want to travel to</Subtitle>
-        <MainHeading>SPACE</MainHeading>
-        <Description>
-          Let’s face it; if you want to go to space, you might as well genuinely go to
-          outer space and not hover kind of on the edge of it. Well sit back, and relax
-          because we’ll give you a truly out of this world experience!
-        </Description>
+        <Hero>
+          <Texts>
+            <Subtitle>So, you want to travel to</Subtitle>
+            <MainHeading>SPACE</MainHeading>
+            <Description>
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
+            </Description>
+          </Texts>
+          <ExploreButtonContainer>Explore Button</ExploreButtonContainer>
+        </Hero>
       </Background>
     </>
-  )
+  );
 }
-const TextStyles = styled`
-  h1 {
 
-}
-`
+const Hero = styled.div`
+  display: flex;
+  padding: 128px 165px;
+`;
 
-const Heading3 = styled.h3`
+const Texts = styled.div`
+  flex: 1;
+`;
 
-`
+const ExploreButtonContainer = styled.div`
+  flex: 1;
+  color: var(--white); // temp
+`;
 
 const Description = styled.p`
   color: var(--blue-300);
-`
+`;
 
 const MainHeading = styled.h1`
   color: var(--white);
-`
+`;
 
 const Subtitle = styled.h3`
   color: var(--blue-300);
-`
+`;
 
 const Background = styled.main`
   height: 100vh;
@@ -48,13 +59,13 @@ const Background = styled.main`
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
-  background-image: url('/assets/home/background-home-desktop.jpg');
+  background-image: url("/assets/home/background-home-desktop.jpg");
 
   @media ${QUERIES.tabletAndDown} {
-    background-image: url('/assets/home/background-home-tablet.jpg');
+    background-image: url("/assets/home/background-home-tablet.jpg");
   }
 
   @media ${QUERIES.mobileAndDown} {
-    background-image: url('/assets/home/background-home-mobile.jpg');
+    background-image: url("/assets/home/background-home-mobile.jpg");
   }
-`
+`;
