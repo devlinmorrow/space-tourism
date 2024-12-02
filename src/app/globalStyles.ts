b627a220c3b13ea8c15daeed6e1883a429aa0eb9
Devlin Morrow <devlin.glasman@gmail.com>
2024-12-02 13:16:14 +0100

    Add Josh's CSS reset

--- src/app/globalStyles.ts
+++ src/app/globalStyles.ts
@@ -3,36 +3,80 @@ import { createGlobalStyle, css } from "styled-components";
 // TODO: Bellefair Regular - Preset 1, 2, 3, 4, 6
 // TODO: Barlow Condensed Regular - Preset 5, 7, 8, 9
 const GlobalStyle = createGlobalStyle`
+  /* 1. Use a more-intuitive box-sizing model */
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  /* 2. Remove default margin */
+  * {
+    margin: 0;
+  }
+
+  body {
+    /* 3. Add accessible line-height */
+    line-height: 1.5;
+    /* 4. Improve text rendering */
+    -webkit-font-smoothing: antialiased;
+  }
+
+  /* 5. Improve media defaults */
+  img, picture, video, canvas, svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  /* 6. Inherit fonts for form controls */
+  input, button, textarea, select {
+    font: inherit;
+  }
+
+  /* 7. Avoid text overflows */
+  p, h1, h2, h3, h4, h5, h6 {
+    overflow-wrap: break-word;
+    line-height: normal;
+  }
+
+  /* 8. Improve line wrapping */
+  p {
+    text-wrap: pretty;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    text-wrap: balance;
+  }
+
+  /*
+		9. Create a root stacking context
+	*/
+  #root, #__next {
+    isolation: isolate;
+  }
+
   h1 {
     font-size: 144px;
-    line-height: normal;
   }
 
   h2 {
     font-size: 100px;
-    line-height: normal;
   }
 
   h3 {
     font-size: 56px;
-    line-height: normal;
   }
 
   h4 {
     font-size: 32px;
-    line-height: normal;
   }
 
 
   h5 {
     font-size: 28px;
-    line-height: normal;
     letter-spacing: 4px;
   }
 
   h6 {
     font-size: 28px;
-    line-height: normal;
   }
 `;
 
